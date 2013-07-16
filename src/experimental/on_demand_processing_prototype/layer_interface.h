b818e21b6518fb47f18507e458a3eeb12c543ff8
Olgierd Humenczuk <olgierdh@gmail.com>
2013-07-16 21:34:53 +0200

    tabs->spaces

--- src/experimental/on_demand_processing_prototype/layer_interface.h
+++ src/experimental/on_demand_processing_prototype/layer_interface.h
@@ -2,122 +2,124 @@
 #define __LAYER_INTERFACE_H__
 
 /**
- * \file 	layer_interface.h
- * \author 	Olgierd Humenczuk
- * \brief 	Containes the layer interface implementation as long as base structure declarations
- *			required by the layer to work.
+ * \file    layer_interface.h
+ * \author  Olgierd Humenczuk
+ * \brief   Containes the layer interface implementation as long as base structure declarations
+ *          required by the layer to work.
  */
 
 // forward declaration
 struct __matrix_layer;
 
 /**
- * \brief 	layer states it simplifies the between layer communication
- * \note  	This is part of the standarized protocol of communication that 
- * 		  	has been designed in order to provide the minimum restrictions, 
- *			in order to make the usage safe, with maximum capabilities.
+ * \brief   layer states it simplifies the between layer communication
+ * \note    This is part of the standarized protocol of communication that 
+ *          has been designed in order to provide the minimum restrictions, 
+ *          in order to make the usage safe, with maximum capabilities.
  */
 typedef enum
 {
-	LAYER_STATE_OK = 0,
-	LAYER_STATE_FULL,
-	LAYER_STATE_TIMEOUT,
-	LAYER_STATE_ERROR
+    LAYER_STATE_OK = 0,
+    LAYER_STATE_FULL,
+    LAYER_STATE_TIMEOUT,
+    LAYER_STATE_ERROR
 } layer_state_t;
 
 
 /**
- * \brief 	allows to seek for the next call context 
+ * \brief   allows to seek for the next call context 
  */
 typedef struct layer_connectivity
 {
-	struct __matrix_layer* self;
-	struct __matrix_layer* next;
-	struct __matrix_layer* prev;
+    struct __matrix_layer* self;
+    struct __matrix_layer* next;
+    struct __matrix_layer* prev;
 } layer_connectivity_t;
 
 /**
  * \struct layer_interface_t
  * 
- * \brief	The raw interface of the purly raw layer that combines both: simplicity and functionality
- * 	   of the 'on demand processing' idea. 
+ * \brief   The raw interface of the purly raw layer that combines both: simplicity and functionality
+ *     of the 'on demand processing' idea. 
  * 
- *				The design idea is based on the assumption that the communication with the server 
- *	   		is bidirectional and the data is processed in packages. This interface design is 
- *	   		very generic and allows to implement different approaches as an extension of the 
- *     		basic operations that the interface is capable of. In very basic scenario the client 
- *     		wants to send some number of bytes to the server and then awaits for the response. 
- *     		After receiving the response the response is passed by to the processing layer through
- *	   		on_data_ready function which signals the processing layer that there is new data to process.
+ *              The design idea is based on the assumption that the communication with the server 
+ *          is bidirectional and the data is processed in packages. This interface design is 
+ *          very generic and allows to implement different approaches as an extension of the 
+ *          basic operations that the interface is capable of. In very basic scenario the client 
+ *          wants to send some number of bytes to the server and then awaits for the response. 
+ *          After receiving the response the response is passed by to the processing layer through
+ *          on_data_ready function which signals the processing layer that there is new data to process.
  *
- *          	Term 'on demand processing' refers to the processing managed by the connection. 
- *     		With the observation that all of the decision in the software depends on the network 
- *     		capabilities it is clear that only the connection has ability to give the signals about 
- *     		the processing to the rest of the components. The 'demand' takes it source in the 
- *     		connection and that is the layer that has command over the another layers.
+ *              Term 'on demand processing' refers to the processing managed by the connection. 
+ *          With the observation that all of the decision in the software depends on the network 
+ *          capabilities it is clear that only the connection has ability to give the signals about 
+ *          the processing to the rest of the components. The 'demand' takes it source in the 
+ *          connection and that is the layer that has command over the another layers.
  */
 typedef struct layer_interface
 {
-	/**
-	 * \brief the function that is called whenever the prev layer wants more data to process/send over some kind of the connection
-	 */
-	layer_state_t ( *on_demand )( layer_connectivity_t* context, const char* buffer, size_t size );
+    /**
+     * \brief the function that is called whenever the prev layer wants more data to process/send over some kind of the connection
+     */
+    layer_state_t ( *on_demand )( layer_connectivity_t* context, const char* buffer, size_t size );
 
-	/**
-	 * \brief the function that is called whenever there is data that is ready and it's source is the prev layer
-	 */
-	layer_state_t ( *on_data_ready )( layer_connectivity_t* context, char* buffer, size_t size );
+    /**
+     * \brief the function that is called whenever there is data that is ready and it's source is the prev layer
+     */
+    layer_state_t ( *on_data_ready )( layer_connectivity_t* context, char* buffer, size_t size );
 
-	/**
-	 * \brief whenever the processing chain suppose to be closed
-	 */
-	layer_state_t ( *close )( layer_connectivity_t* context );
+    /**
+     * \brief whenever the processing chain suppose to be closed
+     */
+    layer_state_t ( *close )( layer_connectivity_t* context );
 
-	/**
-	 * \brief whenver the processing chain is going to be closed it's source is in the prev layer
-	 */
-	layer_state_t ( *on_close )( layer_connectivity_t* context );
+    /**
+     * \brief whenver the processing chain is going to be closed it's source is in the prev layer
+     */
+    layer_state_t ( *on_close )( layer_connectivity_t* context );
 
 } layer_interface_t;
 
 /**
  * \brief The mirror class that makes the access to the generated types
- * 			possible
+ *          possible
  */
 typedef struct __matrix_layer
 {
-	layer_interface_t 		layer_functions;
-	layer_connectivity_t	layer_connection;
-	void*					user_data;
+    layer_interface_t       layer_functions;
+    layer_connectivity_t    layer_connection;
+    void*                   user_data;
 } __matrix_layer_t;
 
 #define DECLARE_LAYER( layer_name ) typedef struct layer_name\
 {\
-	layer_interface_t 		layer_functions;\
-	layer_connectivity_t	layer_connection;\
-	void*					user_data;\
+    layer_interface_t       layer_functions;\
+    layer_connectivity_t    layer_connection;\
+    void*                   user_data;\
 } layer_name##_t
 
 #define LAYER_INSTANCE( layer_name, layer_name_instance, __on_demand, __on_data_ready, __close, __on_close )\
-	layer_name##_t layer_name_instance = { { __on_demand, __on_data_ready, __close, __on_close }, { 0, 0, 0 } };\
-	void* layer_name_instance##__ptr = &layer_name_instance;\
-	layer_name_instance.layer_connection.self = layer_name_instance##__ptr;\
-	layer_name_instance.user_data = ( void* ) ( intptr_t ) 0x01;
+    layer_name##_t layer_name_instance = { { __on_demand, __on_data_ready, __close, __on_close }, { 0, 0, 0 } };\
+    void* layer_name_instance##__ptr = &layer_name_instance;\
+    layer_name_instance.layer_connection.self = layer_name_instance##__ptr;\
+    layer_name_instance.user_data = ( void* ) ( intptr_t ) 0x01;
 
 #define CONNECT_LAYERS( lp_i, ln_i )\
-	ln_i.layer_connection.prev 	= ( void* ) &lp_i;\
-	lp_i.layer_connection.next 	= ( void* ) &ln_i
+    ln_i.layer_connection.prev  = ( void* ) &lp_i;\
+    lp_i.layer_connection.next  = ( void* ) &ln_i
 
 #define LAYER_GET_CONTEXT_PTR( instance )\
-	&instance.layer_connection
+    &instance.layer_connection
 
+// ON_DEMAND
 #define CALL_ON_SELF_ON_DEMAND( context, buffer, size )\
-	context.layer_connection.self->layer_functions.on_demand( ( void* ) &context.layer_connection.self->layer_connection, buffer, size )
+    context.layer_connection.self->layer_functions.on_demand( ( void* ) &context.layer_connection.self->layer_connection, buffer, size )
 
 #define CALL_ON_NEXT_ON_DEMAND( context, buffer, size )\
-	context.layer_connection.next->layer_functions.on_demand( ( void* ) &context.layer_connection.next->layer_connection, buffer, size )
+    context.layer_connection.next->layer_functions.on_demand( ( void* ) &context.layer_connection.next->layer_connection, buffer, size )
 
 #define CALL_ON_PREV_ON_DEMAND( context, buffer, size )\
-	context.layer_connection.prev->layer_functions.on_demand( ( void* ) &context.layer_connection.prev->layer_connection, buffer, size )
+    context.layer_connection.prev->layer_functions.on_demand( ( void* ) &context.layer_connection.prev->layer_connection, buffer, size )
+
 
 #endif
